2105187df542d3f87fd5c0ea5f76096f34c9d769
Daniel Givens <daniel.givens@rackspace.com>
2013-07-06 14:34:21 -0500

    Fixed up the default attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
 
-default['rsdns']['abort_on_failure'] = true
+default['rsdns']['rackspace_username'] = 'your_rackspace_username'
+default['rsdns']['rackspace_api_key'] = 'your_rackspace_api_key'
+default['rsdns']['rackspace_auth_region'] = 'us'
