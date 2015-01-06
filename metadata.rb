39111a871cbb5d34268af323f997bc01dc307cfd
Nick Silkey <nick.silkey@rackspace.com>
2015-01-05 21:27:02 -0600

    Version bump 1.1.0.

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'daniel.givens@rackspace.com'
 license          'Apache 2.0'
 description      'Manages DNS records and zones in Rackspace Cloud DNS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 
