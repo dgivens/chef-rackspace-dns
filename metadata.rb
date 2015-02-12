683cc951ddf4181a7c14deb9f0202588fefb5000
Daniel Givens <daniel.givens@rackspace.com>
2015-02-12 17:34:42 -0600

    Version bump to 1.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'daniel.givens@rackspace.com'
 license          'Apache 2.0'
 description      'Manages DNS records and zones in Rackspace Cloud DNS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.1'
+version          '1.1.2'
 
